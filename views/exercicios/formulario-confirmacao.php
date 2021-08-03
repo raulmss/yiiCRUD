fc332d5f7b2881b15abcdb65fa1c615de85bf83a
raulmss <raul.soutos96@gmail.com>
2021-08-02 22:35:36 -0300

    exclusão de arquivos temporários para estudo

--- views/exercicios/formulario-confirmacao.php
+++
@@ -1,8 +0,0 @@
-<p>Informações inseridas no Cadastro</p>
-
-<ul>
-    <il><label>Nome</label>:<?=$model->nome?></il>
-    <il><label>E-mail</label>:<?=$model->email?></il>
-    <il><label>Idade</label>:<?=$model->idade?></il>
-</ul>
-<?php
